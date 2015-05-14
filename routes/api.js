055c79fed24c537bc103b722ff4ab6d90244bc38
Adam Schodde <aaschodd@asu.edu>
2015-05-14 14:35:48 -0700

    Removed extra zz values.

--- routes/api.js
+++ routes/api.js
@@ -41,7 +41,7 @@ router.post('/archive', function (req, res) {
         var latestListing = listings[0];
         var latestListingHuman = latestListing.time_ago;
         var latestListingDate = moment(latestListing.published_at).utc().format('dddd, MMMM Do YYYY');
-        var latestListingTime = moment(latestListing.published_at).utc().format('hA zz');
+        var latestListingTime = moment(latestListing.published_at).utc().format('hA');
         alexa.response('The latest archive is dated as ' + latestListingHuman + ' on ' + latestListingDate + ' at ' + latestListingTime + ' UTC.', {
           title: 'Bassdrive',
           subtitle: 'Latest Archive',
